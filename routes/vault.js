e2a39763011daf40afde73cf9b8c826cc8beaf3b
BLUEDERPY <jacobbarshaw@gmail.com>
2023-03-07 17:16:26 -0500

    update

--- routes/vault.js
+++ routes/vault.js
@@ -1,6 +1,8 @@
 const { Worker } = require("worker_threads");
+var CronJob = require('cron').CronJob;
 
-const vaultRoutes = (app, fs) => {
+  const vaultRoutes = (app, fs) => {
+
     //...unchanged ^^^
   
     // refactored helper methods
@@ -19,7 +21,33 @@ const vaultRoutes = (app, fs) => {
         callback(returnJson ? JSON.parse(data) : data);
       });
     };
+    app.ws('/echo', function(ws, req) {
+      let length = 0;
+      fs.readFile('./data/prices.json', 'utf8', (err, data) => {
+          const _data = (JSON.parse(data)).prices;
+          length = _data.length;
+          ws.send(JSON.stringify(_data));
+      }, true);
 
+      const job2 = new CronJob('*/5 * * * * *', function() {
+          console.log("BrodcastStart")
+          fs.readFile('./data/prices.json', 'utf8', (err, data) => {
+            const _data = (JSON.parse(data)).prices;
+            if(_data.length > length){
+              length = _data.length;
+              ws.send(JSON.stringify(_data));
+            }
+          }, true);
+
+      });
+      ws.onclose = () =>{
+        console.log("closed");
+        job2.stop();
+      }
+      
+      job2.start();
+
+    });
     // READ
     // Notice how we can make this 'read' operation much more simple now.
     app.get('/vault', (req, res) => {
@@ -31,6 +59,13 @@ const vaultRoutes = (app, fs) => {
             }, true);
         })
     });
+    app.get('/prices', (req, res) => {
+      fs.readFile('./data/prices.json', 'utf8', (err, file) => {
+          const data = JSON.parse(file);
+              console.log(req.body);
+              res.send(data);
+      })
+  });
 
     app.post("/vault", async (req, res) => {
         const worker = new Worker("./workers/worker.js");
--- workers/worker.js
+++ workers/worker.js
@@ -108,45 +108,7 @@ const updateSlugs =
     })
 
 }
-const getPriceData = async (lastBlock, newLastblock) => {
-  
-    const _infuraProvider = new ethers.ethers.getDefaultProvider('goerli', {
-        etherscan: 'A7257M9X55EPEKRDUNV1Q4JH65YM7NKHIT',
-        infura: '28c9e86d104c418384155940687a2b9b',
-        // alchemy: 'g1GPr8Zqbwwnnc14xhraypg1rWB-1Neq',
-        alchemy: 'fIsvO5eINfwXeT-GPtBFBLoXLZSwyjAx', // main
-        // pocket: '95f40ef6a91572c9d6fdfc4b2c74523519186ba6cead77c4a0a87c2dc7110a33'
-        pocket: '569b9d450a6822d605df887c8ccb798c4605e0bf246944a34ef8d11d2d900365' // main
-      })
-    
-      const _jayContract = new ethers.ethers.Contract(
-        JAY_CONTRACT[5].address,
-        JAY_CONTRACT[5].abi,
-        _infuraProvider
-      );
 
-      const eventFilter = _jayContract.filters.Price();
-    
-      const events = await _jayContract.queryFilter(eventFilter, lastBlock, newLastblock);
-
-
-
-      const arrays = [];
-      events.forEach((o) => {
-        //console.log(o.args.time)
-        const date = Number(o.args.time);
-        //console.log(date)
-        const sent = Number(ethers.ethers.formatEther(o.args.sent))?.toFixed(8) || 0;
-        //console.log(sent)
-        const rec = Number(ethers.ethers.formatEther(o.args.recieved))?.toFixed(8) || 1;
-        //console.log(rec)
-        const price = sent / rec;
-        arrays.push([date, price, price, price, price]);
-        return null;
-      });
-    
-      return arrays;
-}
 const getAlchemyCollections =
     async (_data, _vaultOpensea, _pageKey) => {
         return new Promise(resolve => {
@@ -207,16 +169,14 @@ fs.readFile('./data/vault.txt', 'utf8', (err, file) => {
                         if (date - new Date(data.lastUpdate) > FIVE_MIN) {
                             const _dataa = await getOpenseaCollections(0);
                             await updateSlugs(_dataa);
-                            const lastBlock = await _infuraProvider.getBlockNumber();
-                            const prices = await getPriceData(data.lastBlock, lastBlock);
+                            //const lastBlock = await _infuraProvider.getBlockNumber();
+                            //const prices = await getPriceData(data.lastBlock, lastBlock);
                            
                             const alchemyCollections = await getAlchemyCollections(_dataa,[],0)
 
                             const _data = {
                                 lastUpdate: date.toISOString(),
-                                lastBlock,
                                 collections: alchemyCollections,
-                                prices: [...data.prices, ...prices]
                             }
                             writeFile(JSON.stringify(_data, null, 2), () => {
                                 fs.writeFile('./data/vault.txt', "done", async (file2) => {})
